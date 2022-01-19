0b0d92f4625eec71b41d7fd6e2aa5eba8768de7f
imel123 <itorra18@epoka.edu.al>
2022-01-19 17:42:07 +0100

    Convert Dropdown Search Translate components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
-export default () =>{
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a frontend Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+const options =[
+    {
+        label: 'The color Red',
+        value: 'red'
+    },
+    {
+        label: 'The color Green',
+        value: 'green'
+    },
+    {
+        label:'A Shade of Blue',
+        value: 'blue'
+    }
+]
+
+const showAccordion = () => {
+    if (window.location.pathname === '/'){
+        return <Accordion />
+    }
+    
+}
+
+
+export default () => {
     return (
-        <div> 
-            <Accordion /> 
+        <div>
+            {showAccordion()}
+            {/* <Translate /> */}
         </div>
     )
 };
+
