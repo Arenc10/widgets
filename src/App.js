cb20fcf6f5092cf8e80c0c5c46d78e4971ced114
imel123 <itorra18@epoka.edu.al>
2022-01-20 10:16:41 +0100

    Adding navigation links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 
 
@@ -38,42 +39,30 @@ const options =[
     }
 ]
 
-const showAccordion = () => {
-    if (window.location.pathname === '/'){
-        return <Accordion items={items} />
-    }
-    
-};
-
-const showList = () => {
-    if (window.location.pathname === '/list'){
-        return <Search />;
-    }
-};
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown'){
-        return <Dropdown />
-    }
-};
-
-const showTranslate = () => {
-    if (window.location.pathname === '/translate'){
-        return <Translate />
-    }
-}
-
-
 
 export default () => {
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
+            <Header />
             <Route path = "/">
                 <Accordion items = {items} />
             </Route>
             <Route path = "/list">
                 <Search />
             </Route>
+            <Route path = "/dropdown">
+                <Dropdown
+                label= "Select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path = "/translate">
+                <Translate />
+            </Route>
+            
         </div>
     )
 };
